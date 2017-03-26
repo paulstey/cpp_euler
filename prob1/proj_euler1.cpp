f61da2f061cfb6ebe516412ac9a83e14a9e8faa1
paulstey <paulstey@gmail.com>
2017-03-25 23:05:30 -0400

    first commit

---
+++ prob1/proj_euler1.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+
+// Project Euler: Problem 1
+// Take sum of all integers that are multiples of 3 or 5
+// Date: 2017-03-11
+// Author: Paul Stey
+// Compile with: g++ proj_euler1.cpp -Wall -o prob1
+
+bool is_case(int n) {
+    bool res = false;
+    
+    if (n % 3 == 0) {
+	res = true;
+    }
+    else if (n % 5 == 0) {
+	res = true;
+    }
+    return res;
+}
+
+
+int get_maxval() {
+    int res;
+    std::cout << "Enter maxval: " << std::endl;
+    std::cin >> res;
+    return res;
+}
+
+
+int main() {
+    int res = 0;
+    int maxval = get_maxval();
+	
+    for (int i = 3; i < maxval; ++i) {
+	if (is_case(i)) {
+	    res += i;
+	}
+    }
+    std::cout << "Solution: " << res << std::endl;
+}
