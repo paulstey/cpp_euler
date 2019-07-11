bf11b7db4d58e6577df511118fe5f7d163a3d5d7
paulstey <paulstey@gmail.com>
2019-07-11 10:16:32 -0400

    refactor: change newline to comma and space

--- prob5/prob5.cpp
+++ prob5/prob5.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <vector>
+
 #include "factors.h"
 
 int main() {
     std::vector<uint64_t> res = factors(2520);
     int n = res.size();
     for (size_t i = 0; i < n; i++) {
-        std::cout << res[i] << "\n";
+        std::cout << res[i] << ", ";
     }
+    std::cout << "\n" << std::endl;
 }
